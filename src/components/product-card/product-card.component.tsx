6318705b19e29263736b5ef32459ed93e46729aa
lysaphan <lysandra.phan@gmail.com>
2024-07-11 00:36:48 +0700

    Separate UI style for ProductCard in HomePage & in CategoryPage

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -28,76 +28,150 @@ export default function ProductCard({
       transform: "translateY(-3px)",
     },
   };
-  
+
   // -------------------------- FUNCTION --------------------------
   function displayPrice(price: number) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-  // -------------------------- MAIN --------------------------
-  return (
-    <Stack
-      spacing={1}
-      sx={stackSX}
-      px={2}
-      pb={2}
-      border={isInCategory ? "2px solid #E4E7E9" : "none"}
-      borderRadius={isInCategory ? 2 : 0}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        width="inherit"
-        height={200}
-      >
-        <CustomImage
-          src={product.imagePath}
-          alt={product.name}
-          width="60%"
-          height="60%"
-        />
-      </Box>
-      <Typography
-        fontSize={16}
-        fontWeight={500}
-        sx={{
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          display: "-webkit-box",
-          WebkitLineClamp: "2",
-          WebkitBoxOrient: "vertical",
-        }}
-      >
-        {product.name}
-      </Typography>
-      <Typography fontSize={16} color="secondary" fontWeight={500}>
-        ${displayPrice(product.price)}
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          fontSize: 1,
-          mt: 0.7,
-        }}
-      >
-        <Rating
-          name="text-feedback"
-          value={product.ratingScore}
-          readOnly
-          precision={0.5}
-          sx={{ fontSize: 20 }}
-          emptyIcon={
-            <StarRateRoundedIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-          }
-        />
-        <Box sx={{ ml: 0.7, mb: -0.3, fontSize: 14 }}>
-          ({product.rateTotal})
+  // -------------------------- RENDER --------------------------
+  const renderProductCardNoBorder = () => {
+    return (
+      <Stack spacing={1} sx={stackSX} px={2} pb={2}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          width="inherit"
+          height={200}
+        >
+          <CustomImage
+            src={product.imagePath}
+            alt={product.name}
+            width="70%"
+            height="70%"
+          />
         </Box>
-      </Box>
-    </Stack>
-  );
+        <Typography
+          fontSize={16}
+          fontWeight={500}
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            WebkitLineClamp: "2",
+            WebkitBoxOrient: "vertical",
+          }}
+        >
+          {product.name}
+        </Typography>
+        <Typography fontSize={16} color="secondary" fontWeight={500}>
+          ${displayPrice(product.price)}
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            fontSize: 1,
+            mt: 0.7,
+          }}
+        >
+          <Rating
+            name="text-feedback"
+            value={product.ratingScore}
+            readOnly
+            precision={0.5}
+            sx={{ fontSize: 20 }}
+            emptyIcon={
+              <StarRateRoundedIcon
+                style={{ opacity: 0.55 }}
+                fontSize="inherit"
+              />
+            }
+          />
+          <Box sx={{ ml: 0.7, mb: -0.3, fontSize: 14 }}>
+            ({product.rateTotal})
+          </Box>
+        </Box>
+      </Stack>
+    );
+  };
+
+  // Render Product Card in Category Page
+  const renderProductCard = () => {
+    return (
+      <Stack
+        spacing={1}
+        sx={stackSX}
+        px={2}
+        pb={2}
+        border="2px solid #E4E7E9"
+        borderRadius={2}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          width="inherit"
+          height={180}
+        >
+          <CustomImage
+            src={product.imagePath}
+            alt={product.name}
+            width="60%"
+            height="50%"
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            fontSize: 1,
+            mt: 0.7,
+          }}
+        >
+          <Rating
+            name="text-feedback"
+            value={product.ratingScore}
+            readOnly
+            precision={0.5}
+            sx={{ fontSize: 15 }}
+            emptyIcon={
+              <StarRateRoundedIcon
+                style={{ opacity: 0.55 }}
+                fontSize="inherit"
+              />
+            }
+          />
+          <Box sx={{ ml: 0.7, mb: -0.3, fontSize: 10, color: "#808080" }}>
+            ({product.rateTotal})
+          </Box>
+        </Box>
+
+        <Typography
+          fontSize={12}
+          fontWeight={500}
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            WebkitLineClamp: "2",
+            WebkitBoxOrient: "vertical",
+          }}
+        >
+          {product.name}
+        </Typography>
+        <Typography fontSize={14} color="secondary" fontWeight={500}>
+          ${displayPrice(product.price)}
+        </Typography>
+      </Stack>
+    );
+  };
+
+  // -------------------------- MAIN --------------------------
+  return isInCategory ? renderProductCard() : renderProductCardNoBorder();
 }
