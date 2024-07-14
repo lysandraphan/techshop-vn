95f28ac8bf7c0a7b75c882a632446a74c4140c0a
lysaphan <lysandra.phan@gmail.com>
2024-07-15 04:44:07 +0700

    Working on ProductDetail Page. Finish fetching product data.

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,8 @@
-
 export const bestSellingApi =
-"https://g5-likelion-ecommerce.onrender.com/api/product/public/best-selling";
+  "https://g5-likelion-ecommerce.onrender.com/api/product/public/best-selling";
 
 export const newArrivalApi =
-"https://g5-likelion-ecommerce.onrender.com/api/product/public/new-arrival";
+  "https://g5-likelion-ecommerce.onrender.com/api/product/public/new-arrival";
 
 export const bannersApi =
   "https://g5-likelion-ecommerce.onrender.com/api/banner-images/public/all";
@@ -11,11 +10,16 @@ export const bannersApi =
 export const categoriesApi =
   "https://g5-likelion-ecommerce.onrender.com/api/categories/public/all";
 
-export const brandsApi = "https://g5-likelion-ecommerce.onrender.com/api/brand/public/all"
+export const brandsApi =
+  "https://g5-likelion-ecommerce.onrender.com/api/brand/public/all";
 
 export const getProductsByCategoryApi = (
   categoryId: number,
   pageNumber: number = 1
 ) => {
   return `https://g5-likelion-ecommerce.onrender.com/api/product/public/${categoryId}/paginate?page=${pageNumber}`;
+};
+
+export const getProductDetailApi = (productId: number) => {
+  return `https://g5-likelion-ecommerce.onrender.com/api/product/public/${productId}`;
 };
--- src/app/categories/[categoryName]/[categoryID]/products/[productID]/page.tsx
+++ src/app/categories/[categoryName]/[categoryID]/products/[productID]/page.tsx
@@ -1,7 +1,112 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import NextLink from "next/link";
+import axios from "axios";
 
+// internal
+import { ProductData } from "@/components/product-list/product-list.component";
+import { getProductDetailApi } from "@/api";
+import { useParams, usePathname, useRouter } from "next/navigation";
+
+// mui
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Grid from "@mui/material/Grid";
+import LoadingFallback from "@/components/loading-fallback/loading-fallback.component";
+import { getCategoryRoute } from "@/redux/features/categories-slice";
+import CustomImage from "@/components/custom-image/custom-image.component";
+
+// EXPORT DEFAULT
 export default function ProductDetail() {
+  // -------------------------- STATE --------------------------
+  const [product, setProduct] = useState<ProductData>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // -------------------------- VAR --------------------------
+  const params = useParams<{
+    categoryName: string;
+    categoryID: string;
+    productID: string;
+  }>();
+  const productId = parseInt(params.productID);
+
+  const api = getProductDetailApi(productId);
+
+  const router = useRouter();
+  const pathName = usePathname();
+
+  // -------------------------- FUNCTION --------------------------
+  const fetchProductDetail = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(api);
+      const result = response.data as ProductData;
+      console.log(result);
+      setProduct(result);
+      setIsLoading(false);
+    } catch (error: any) {
+      setIsLoading(false);
+      console.log(error.message);
+    }
+  };
+
+  // -------------------------- EFFECT --------------------------
+  useEffect(() => {
+    fetchProductDetail();
+  }, []);
+
+  // -------------------------- MAIN --------------------------
+  if (isLoading) return <LoadingFallback />;
   return (
-    <div>ProductDetail</div>
-  )
+    <Container>
+      <Breadcrumbs aria-label="breadcrumb" sx={{ mt: 5 }}>
+        <Link underline="hover" color="inherit" component={NextLink} href="/">
+          Home
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          component={NextLink}
+          href={getCategoryRoute(
+            params.categoryName,
+            parseInt(params.categoryID)
+          )}
+        >
+          {product.categoryDto.name}
+        </Link>
+        <Link
+          underline="hover"
+          color="text.primary"
+          aria-current="page"
+          component={NextLink}
+          href={`${pathName}`}
+        >
+          {product.name}
+        </Link>
+      </Breadcrumbs>
+      <Grid container spacing={5} mt={1} mb={5}>
+        <Grid item md={6}>
+          <CustomImage
+            src={product.imagePath}
+            alt={product.name}
+            height={350}
+            mt={50}
+          />
+          {product.imagesPath &&
+            product.imagesPath.map((image, index) => (
+              <CustomImage
+                key={index}
+                src={image}
+                alt={product.name}
+                height={150}
+              />
+            ))}
+        </Grid>
+        <Grid item md={6}>
+          Product Detail Section
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
