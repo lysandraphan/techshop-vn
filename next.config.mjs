d48f974cbb0c1ae5767d8f3e674a7d4b80902a62
lysaphan <lysandra.phan@gmail.com>
2024-07-15 22:32:35 +0700

    Disable react Strict Mode (prevent messing up Loading state when fetching data)

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'res.cloudinary.com',
-          },
-        ],
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
       },
+    ],
+  },
 };
 
 export default nextConfig;
