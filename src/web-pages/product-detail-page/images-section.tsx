9c3744e7baa3b2b901bc03aa094aff3e41c12126
lysaphan <lysandra.phan@gmail.com>
2024-07-18 02:31:40 +0700

    ProductDetail | Swap large image when clicking small images

--- src/web-pages/product-detail-page/images-section.tsx
+++ src/web-pages/product-detail-page/images-section.tsx
@@ -1,30 +1,53 @@
+import { Fragment, useState } from "react";
+
 // mui
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 
 // component
 import CustomImage from "@/components/custom-image/custom-image.component";
-import { Fragment } from "react";
 
 // interface
 interface ImageSectionProps {
   mainImage: string;
-  images: string[];
+  otherImages: string[];
   name: string;
 }
 
+// EXPORT DEFAULT
 export default function ImageSection({
   mainImage,
-  images,
+  otherImages,
   name,
 }: ImageSectionProps) {
+  // -------------------------- STATE --------------------------
+  const [largeImage, setLargeImage] = useState(mainImage);
+  const [sideImages, setSideImages] = useState(otherImages);
+
+  // -------------------------- FUNCTION --------------------------
+  const swapImagesHandler = (selected: string, index: number) => {
+    setLargeImage(selected);
+    setSideImages((prev) => {
+      prev[index] = largeImage;
+      return prev;
+    });
+  };
+
+  // -------------------------- MAIN --------------------------
   return (
     <Fragment>
       <Grid item md={2}>
         <Stack direction="column" spacing={5}>
-          {images &&
-            images.map((image, index) => (
-              <CustomImage key={index} src={image} alt={name} width={110} height={85} />
+          {sideImages &&
+            sideImages.map((image, index) => (
+              <Box
+                key={image}
+                onClick={() => swapImagesHandler(image, index)}
+                sx={{ cursor: "pointer" }}
+              >
+                <CustomImage src={image} alt={name} width={110} height={85} />
+              </Box>
             ))}
         </Stack>
       </Grid>
@@ -36,7 +59,9 @@ export default function ImageSection({
         alignSelf="center"
         justifyItems="center"
       >
-        <CustomImage src={mainImage} alt={name} width="80%" height={350} />
+        {largeImage && (
+          <CustomImage src={largeImage} alt={name} width="80%" height={350} />
+        )}
       </Grid>
     </Fragment>
   );
