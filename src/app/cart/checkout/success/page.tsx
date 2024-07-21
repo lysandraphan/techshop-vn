7c41ddfdbfd35805bbd810e1fef940e71c86fd5e
lysaphan <lysandra.phan@gmail.com>
2024-07-21 08:36:28 +0700

    Finish NotFoundPage & Modify style of OrderSuccessPage

--- src/app/cart/checkout/success/page.tsx
+++ src/app/cart/checkout/success/page.tsx
@@ -2,17 +2,17 @@
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import Divider from "@mui/material/Divider";
 import Container from "@mui/material/Container";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import HomeIcon from "@mui/icons-material/Home";
 import ChecklistRtlIcon from "@mui/icons-material/ChecklistRtl";
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 
 export default function OrderSuccess() {
   return (
     <Container>
       <Stack justifyContent="center" alignItems="center" mt={10}>
-        <CheckCircleIcon color="secondary" sx={{ fontSize: 80 }} />
+        <CheckCircleOutlineOutlinedIcon sx={{ fontSize: 80, color: "#2DB224" }} />
         <Typography
           mt={5}
           mb={2}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+// mui
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import ErrorIcon from "@mui/icons-material/Error";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function Error404() {
   return (
-    <div>Error 404 - Page not found</div>
-  )
+    <Container>
+      <Stack justifyContent="center" alignItems="center" mt={10}>
+        <ErrorIcon color="secondary" sx={{ fontSize: 80 }} />
+        <Typography
+          mt={5}
+          mb={2}
+          fontSize={20}
+          fontWeight={500}
+          textAlign="center"
+        >
+          Page Not Found
+        </Typography>
+        <Typography mb={5} textAlign="center">
+          The page you try to access cannot be found. Please try another page.
+        </Typography>
+        <Button
+          variant="outlined"
+          color="secondary"
+          sx={{ py: 1, px: 5 }}
+          startIcon={<HomeIcon />}
+        >
+          Home Page
+        </Button>
+      </Stack>
+    </Container>
+  );
 }
